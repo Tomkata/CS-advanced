1798e7f742730a5abacfd6bf85f24e4e0542ae75
Toma Andreev <86717095+Tomkata@users.noreply.github.com>
2024-05-14 01:01:15 +0300

    1.ReverseaString.cs

---
+++ Stacks and Queues/1. Reverse a String/1.ReverseaString.cs
@@ -0,0 +1,18 @@
+namespace LabStacksandQueues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+
+            foreach (var c in input)
+                stack.Push(c);
+
+            while (stack.Count != 0)
+                Console.Write(stack.Pop());
+
+        }
+    }
+}
